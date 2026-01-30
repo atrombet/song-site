282d61be05dede48f1b302c9016e7cbde1416767
Anthony Trombetta <atrombet@gmail.com>
2026-01-30 10:18:45 -0600

    wip: start page for you are on the throne.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,7 @@
 <template>
-  <h1>Song Site</h1>
-  <NuxtLink to="/none-so-great">None So Great</NuxtLink>
+  <div class="flex-column">
+    <h1>Song Scope</h1>
+    <NuxtLink to="/none-so-great">None So Great</NuxtLink>
+    <NuxtLink to="/you-are-on-the-throne">You Are On The Throne</NuxtLink>
+  </div>
 </template>
---
+++ app/pages/you-are-on-the-throne.vue
@@ -0,0 +1,51 @@
+<script setup></script>
+
+<template>
+  <h1 class="ma-0">You Are On The Throne</h1>
+  <h2 class="author">by Anthony Trombetta and David Mace</h2>
+  <audio
+    controls
+    src="https://hslkazhfaubksokrytbz.supabase.co/storage/v1/object/sign/songs/You%20Are%20On%20The%20Throne%20-%20Demo%20v1.1.mp3?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9mNmY4NGJmYS05NzE4LTQ5MDEtYWUwZC1lYTJjYmY1YjAyYTEiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJzb25ncy9Zb3UgQXJlIE9uIFRoZSBUaHJvbmUgLSBEZW1vIHYxLjEubXAzIiwiaWF0IjoxNzY5NTMyNTAzLCJleHAiOjE4MDEwNjg1MDN9.-_LcB2ZnJd-AJHz6bvuQtGUoO-GYlSJQQn4M6VSI1os"
+  />
+  <div class="lyrics mt-xl">
+    <h3 class="mt-0">Verse 1</h3>
+    <div>At the start, there was the Word</div>
+    <div>And the Word was with God, and the Word was God</div>
+    <div>Through Him, all things were made</div>
+    <div>And in Him was life, and that life was the light of man</div>
+
+    <h3>Verse 2</h3>
+    <div></div>
+
+    <h3>Pre-Chorus</h3>
+    <div>At the right hand of the Father you reign</div>
+
+    <h3>Chorus</h3>
+    <div>Name above all names, Jesus</div>
+    <div>High and lifted up forever</div>
+    <div>All your people sing</div>
+    <div>Jesus, you are on the throne</div>
+    <div>Yes, you are on the throne</div>
+
+    <h3>Verse 3</h3>
+
+    <h3>Pre-Chorus</h3>
+
+    <h3>Chorus</h3>
+
+    <h3>Bridge</h3>
+    <div>Crucified, brought back to life</div>
+    <div>Fully man, and all divine</div>
+    <div>Crucified, brought back to life</div>
+    <div>Fully man, and all divine</div>
+    <div>Seated now upon the throne</div>
+    <div>We bend our knees to you alone</div>
+
+    <h3>Chorus</h3>
+  </div>
+
+  <div class="mt-xl">
+    <h2>About this song...</h2>
+    <p></p>
+  </div>
+</template>
